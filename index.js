4452a3a89eacd225f22a5cba1fd63792f26d9f84
Malone <107812723+ezeobinna@users.noreply.github.com>
2023-11-06 00:46:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+const buttonRedact = document.getElementById('btn_redact');
+
+
+
+buttonRedact.addEventListener('click', function () {
+    const wordsToScramble = document.getElementById("scrambled").value.split(/[\s,]+/);
+    const textToRedact = document.getElementById("text_box").value;
+    const scramble = document.getElementById("asterisks").value || '*';
+
+
+    let redactedText = textToRedact;
+    let wordsScanned = 0;
+    let wordsMatched = 0;
+    let characterScrambled = 0;
+
+    wordsToScramble.forEach((word) => {
+        const regex = new RegExp(`\\b${word}\\b`, "gi");
+        redactedText = redactedText.replace(regex, (match) => {
+
+            wordsScanned = textToRedact.trim().split(/\s+/).length;
+            if (match.toLowerCase() === word.toLowerCase() && scramble.length > 1) {
+                wordsMatched++;
+                characterScrambled += match.length;
+                return scramble;
+            } else if (
+                match.toLowerCase() === word.toLowerCase() &&
+                scramble.length === 1
+            ) {
+                wordsMatched++;
+                characterScrambled += match.length;
+                return scramble.repeat(match.length);
+            } else {
+                return match;
+            }
+        });
+    });
+
+    
+    document.getElementById("sn").innerHTML = `${wordsScanned}`;
+    document.getElementById("wm").innerHTML = `${wordsMatched}`;
+    document.getElementById("cs").innerHTML = `${characterScrambled}`;
+
+
+    document.getElementById("redactedOutput").textContent = redactedText;
+});
+
+
